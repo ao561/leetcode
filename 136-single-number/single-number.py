095bd4fd6203f2e4d01382ccbca9c8bbbac93f9c
Amaan Omar <ao561@cam.ac.uk>
2025-08-14 20:11:52 +0100

    Time: 3 ms (65.22%) | Memory: 19.8 MB (21.10%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        map = {}
+
+        for num in nums:
+            map[num] = map.get(num, 0) + 1
+        
+        for key, val in map.items():
+            if val == 1:
+                return key
+    
